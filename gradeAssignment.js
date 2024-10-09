100d5363bc733f0b0836e3efd543d9dde53fd62b
patricia145 <patriciavaldez0517@gmail.com>
2024-10-09 19:38:19 +0800

    initial commit

---
+++ gradeAssignment.js
@@ -0,0 +1,12 @@
+function enterGrade() {
+    let grade = prompt("Enter your score: ");
+    if (grade >= 90) {
+        alert("Your grade is: Excellent.");
+    } else if (grade >= 80) {
+        alert("Your grade is: Good.")
+    } else if (grade >= 70) {
+        alert("Your grade is: Fair.")
+    } else {
+        alert("Your grade: Needs improvement.")
+    }
+}
